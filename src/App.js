836c65e751bfb4e422f40fa020173ba1af138f32
srikanth-vanam <B172320-@rgukt.ac.in>
2023-09-06 19:57:51 +0530

    fixed the cart problems

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
-import { uiActions } from "./store/store";
+import { getData, sendData } from "./store/store";
 
 let initial=true;
 function App() {
@@ -24,54 +24,18 @@ function App() {
   ];
 
   const cartItems = useSelector((state) => state.cart.cartItems);
-  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        dispatch(
-          uiActions.setNotification({
-            status: "pending",
-            title: "sending",
-            message: "sending cart data",
-          })
-        );
-        const resp = await fetch(
-          "https://expense-tracker-fea86-default-rtdb.firebaseio.com/cartItems.json",
-          {
-            method: "PUT",
-            body: JSON.stringify(cartItems),
-          }
-        );
-
-        if (resp.ok) {
-          dispatch(uiActions.setNotification({
-            status: "success",
-              title: "success",
-              message: "succesfully sent cart data",
-          }))
-        } else {
-          dispatch(uiActions.setNotification({
-            status: "error",
-              title: "Error",
-              message: "error in sending cart data",
-          })) 
-        }
-      } catch (error) {
-        dispatch(uiActions.setNotification({
-          status: "error",
-            title: "Error",
-            message: "error in sending cart data",
-        }))
-      }
-    };
     if(initial){
       initial=false;
       return;
     }
-    fetchData();
-  }, [cartItems]);
+    dispatch(sendData(cartItems));
+  }, [cartItems,dispatch]);
 
+  useEffect(()=>{
+    dispatch(getData());
+  },[dispatch])
   return (
     <Layout>
       {isCartShown && <Cart />}
--- src/store/store.js
+++ src/store/store.js
@@ -21,13 +21,13 @@ const uiSlice = createSlice({
   name: "UI",
   initialState: initialUiState,
   reducers: {
-    setNotification(state,action){
-        state.notification={
-            status:action.payload.status,
-            title:action.payload.title,
-            message:action.payload.message,
-        }
-    }
+    setNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
   },
 });
 
@@ -37,6 +37,109 @@ const store = configureStore({
     ui: uiSlice.reducer,
   },
 });
+
+export const sendData = (cartItems) => {
+  return async (dispatch) => {
+    try {
+      dispatch(
+        uiActions.setNotification({
+          status: "pending",
+          title: "sending",
+          message: "sending cart data",
+        })
+      );
+      const resp = await fetch(
+        "https://expense-tracker-fea86-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cartItems),
+        }
+      );
+
+      if (resp.ok) {
+        dispatch(
+          uiActions.setNotification({
+            status: "success",
+            title: "success",
+            message: "succesfully sent cart data",
+          })
+        );
+      } else {
+        dispatch(
+          uiActions.setNotification({
+            status: "error",
+            title: "Error",
+            message: "error in sending cart data",
+          })
+        );
+      }
+    } catch (error) {
+      dispatch(
+        uiActions.setNotification({
+          status: "error",
+          title: "Error",
+          message: "error in sending cart data",
+        })
+      );
+    }
+  };
+};
+
+export const getData = () => {
+  return async (dispatch) => {
+    try {
+      dispatch(
+        uiActions.setNotification({
+          status: "pending",
+          title: "Getting",
+          message: "getting cart data",
+        })
+      );
+      const resp = await fetch(
+        "https://expense-tracker-fea86-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "GET",
+        }
+      );
+
+      if (resp.ok) {
+        const result = await resp.json();
+        console.log("data from get is", result);
+        let items = [];
+        if (result) {
+          for (const item of result) {
+            items.push(item);
+          }
+          dispatch(cartActions.setCartItems(items));
+        }
+
+        dispatch(
+          uiActions.setNotification({
+            status: "success",
+            title: "success",
+            message: "succesfully retrieved data",
+          })
+        );
+      } else {
+        dispatch(
+          uiActions.setNotification({
+            status: "error",
+            title: "Error",
+            message: "error in getting cart data",
+          })
+        );
+      }
+    } catch (error) {
+      dispatch(
+        uiActions.setNotification({
+          status: "error",
+          title: "Error",
+          message: "error in getting cart data",
+        })
+      );
+    }
+  };
+};
 
 export const uiActions = uiSlice.actions;
 export const cartActions = cartSlice.actions;
